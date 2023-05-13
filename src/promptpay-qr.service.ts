820b0609935f4f1368e2fa3684355debcc3569f2
Aung Thiha <aungthiha.160898@gmail.com>
2023-05-14 01:16:39 +0700

    added injectable for service

--- src/promptpay-qr.service.ts
+++ src/promptpay-qr.service.ts
@@ -1,5 +1,7 @@
+import { Injectable } from '@nestjs/common';
 import generatePayload = require('promptpay-qr');
 
+@Injectable()
 export class PromptpayQRService {
   generate(id: string, amount: number): string {
     return generatePayload(id, { amount });
