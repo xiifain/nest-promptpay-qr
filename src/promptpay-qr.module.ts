24581492782103e4652987383b46ffa2bb2781fc
Aung Thiha <aungthiha.160898@gmail.com>
2023-05-14 01:13:26 +0700

    added both providers and exports

--- src/promptpay-qr.module.ts
+++ src/promptpay-qr.module.ts
@@ -3,6 +3,7 @@ import { PromptpayQRService } from './promptpay-qr.service';
 
 @Module({
   imports: [],
+  providers: [PromptpayQRService],
   exports: [PromptpayQRService],
 })
 export class PromptpayQRModule {}
